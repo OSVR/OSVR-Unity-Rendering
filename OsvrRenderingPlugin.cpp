b9a47c3309332c846b07a55d7b43fd8c9629f5e5
DuFF14 <greg.aring@gmail.com>
2015-08-13 22:13:07 -0400

    Moved SetupEye and SetupDisplay, added SetupEye body from D3D 2d example.

--- OsvrRenderingPlugin.cpp
+++ OsvrRenderingPlugin.cpp
@@ -86,7 +86,45 @@ static void SetDefaultGraphicsState();
 
 // --------------------------------------------------------------------------
 // C API and internal function implementation
+// Callback to set up for rendering into a given display (which may have on or more eyes).
+void SetupDisplay(
+	void *userData              //< Passed into SetViewProjectionCallback
+	, osvr::renderkit::GraphicsLibrary   library //< Graphics library context to use
+	)
+{
+	auto context = library.D3D11->context;
+	auto renderTargetView = library.D3D11->renderTargetView;
+	auto depthStencilView = library.D3D11->depthStencilView;
 
+	// Perform a random colorfill
+	FLOAT red = static_cast<FLOAT>((double)rand() / (double)RAND_MAX);
+	FLOAT green = static_cast<FLOAT>((double)rand() / (double)RAND_MAX);
+	FLOAT blue = static_cast<FLOAT>((double)rand() / (double)RAND_MAX);
+	FLOAT colorRgba[4] = { red, green, blue, 1.0f };
+	context->ClearRenderTargetView(renderTargetView, colorRgba);
+	context->ClearDepthStencilView(depthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+}
+
+// Callback to set up for rendering into a given eye (viewpoint and projection).
+void SetupEye(
+	void *userData              //< Passed into SetViewProjectionCallback
+	, osvr::renderkit::GraphicsLibrary   library //< Graphics library context to use
+	, osvr::renderkit::OSVR_ViewportDescription viewport  //< Viewport set by RenderManager
+	, osvr::renderkit::OSVR_ProjectionMatrix  projection  //< Projection matrix set by RenderManager
+	, size_t    whichEye        //< Which eye are we setting up for?
+	)
+{
+	// Nothing to do here -- it has been set up for us.
+	ID3D11DeviceContext *context = library.D3D11->context;
+	ID3D11RenderTargetView *renderTargetView = library.D3D11->renderTargetView;
+
+	// Perform a random colorfill
+	FLOAT red = static_cast<FLOAT>((double)rand() / (double)RAND_MAX);
+	FLOAT green = static_cast<FLOAT>((double)rand() / (double)RAND_MAX);
+	FLOAT blue = static_cast<FLOAT>((double)rand() / (double)RAND_MAX);
+	FLOAT colorRgba[4] = { red, green, blue, 1.0f };
+	context->ClearRenderTargetView(renderTargetView, colorRgba);
+}
 // RenderEvents
 // If we ever decide to add more events, here's the place for it.
 enum RenderEvents { kOsvrEventID_Render = 0 };
@@ -148,37 +186,6 @@ extern "C" OSVR_ReturnCode EXPORT_API CreateRenderManagerFromUnity(OSVR_ClientCo
   }
   DebugLog("[OSVR Rendering Plugin] Success?");
   return OSVR_RETURN_SUCCESS;
-}
-
-// Callback to set up for rendering into a given display (which may have on or more eyes).
-void SetupDisplay(
-	void *userData              //< Passed into SetViewProjectionCallback
-	, osvr::renderkit::GraphicsLibrary   library //< Graphics library context to use
-	)
-{
-	auto context = library.D3D11->context;
-	auto renderTargetView = library.D3D11->renderTargetView;
-	auto depthStencilView = library.D3D11->depthStencilView;
-
-	// Perform a random colorfill
-	FLOAT red = static_cast<FLOAT>((double)rand() / (double)RAND_MAX);
-	FLOAT green = static_cast<FLOAT>((double)rand() / (double)RAND_MAX);
-	FLOAT blue = static_cast<FLOAT>((double)rand() / (double)RAND_MAX);
-	FLOAT colorRgba[4] = { red, green, blue, 1.0f };
-	context->ClearRenderTargetView(renderTargetView, colorRgba);
-	context->ClearDepthStencilView(depthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
-}
-
-// Callback to set up for rendering into a given eye (viewpoint and projection).
-void SetupEye(
-	void *userData              //< Passed into SetViewProjectionCallback
-	, osvr::renderkit::GraphicsLibrary   library //< Graphics library context to use
-	, osvr::renderkit::OSVR_ViewportDescription viewport  //< Viewport set by RenderManager
-	, osvr::renderkit::OSVR_ProjectionMatrix  projection  //< Projection matrix set by RenderManager
-	, size_t    whichEye        //< Which eye are we setting up for?
-	)
-{
-	// Nothing to do here -- it has been set up for us.
 }
 
 // @todo Figure out what should be in here, this code is taken from
